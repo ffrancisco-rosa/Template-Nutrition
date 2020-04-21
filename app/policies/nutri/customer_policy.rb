9da351f4a834131c64dd69bead7cad2b8d581e0b
Francisco Rosa <chicorosa@gmail.com>
2020-04-21 17:22:26 +0100

    pundit

---
+++ app/policies/nutri/customer_policy.rb
@@ -0,0 +1,39 @@
+class Nutri::CustomerPolicy < ApplicationPolicy
+  # [...]
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def edit
+    user_is_nutri?
+  end
+
+  def update
+    user_is_nutri?
+  end
+
+  def destroy
+    user_is_nutri?
+  end
+
+
+  private
+
+  def user_is_nutri?
+    record.user == nutri
+  end
+end
