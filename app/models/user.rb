5bf9a1894346fd2da2d53f28eebc518fb5360cae
Francisco Rosa <chicorosa@gmail.com>
2020-04-23 14:26:33 +0100

    going back to drawing board

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: [:nutri, :guest]
+  enum role: [:guest, :nutri]
   has_many :customers, class_name: 'Customer', foreign_key: "nutri_id"
 end
